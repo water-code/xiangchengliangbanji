08f968e0458533549a268f0aebc69ab93155665e
water-code <168319570@qq.com>
2023-05-18 15:26:06 +0800

    test

--- xiangchengfront/vite.config.js
+++ xiangchengfront/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:8081',   //代理接口
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')  // 重定向
+        rewrite: (path) => path.replace(/^\/api/, '')  // 重定向.
       }
     }
   }
