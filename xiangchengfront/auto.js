dd9987ae1e6cb87287a3d9c5ca131c991b80ddef
water-code <168319570@qq.com>
2023-05-19 20:28:58 +0800

    change auto.js

--- xiangchengfront/auto.js
+++ xiangchengfront/auto.js
@@ -1,6 +1,8 @@
 const http = require("http");	//http模块用来创建http服务
 const createHandler = require("github-webhook-handler");//中间件，用来监听github的webHook的事件
-
+const path = require("path");
+const fs = require("fs");
+const { execSync } = require("child_process");
 const handler = createHandler({
     path: "/webhook",
     secret: "mySecretKiKoHashKey",
@@ -13,5 +15,22 @@ http.createServer(function (req, res) {
 }).listen(8082);//创建http服务并且监听8082端口
 
 handler.on("push", function (event) {
-	console.log(event)
+    const projectDir = path.resolve(`./${event.payload.repository.name}`);
+    const projectDirFront = path.resolve(`./${event.payload.repository.name}/xiangchengfront`);
+
+    //拉取项目前先删除一次项目
+  	if (fs.existsSync(projectDir)) {
+        execSync(`rm -rf ${event.payload.repository.name}`, {
+            stdio: "inherit",
+        });
+    }
+
+    // 拉取仓库最新代码 
+    execSync(
+        `git@github.com:water-code/xiangchengliangbanji.git ${projectDir}`,
+        {
+            stdio: "inherit",
+        }
+    );
+
 })//监听github仓库的push事件
