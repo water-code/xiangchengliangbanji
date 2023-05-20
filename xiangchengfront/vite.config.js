1dec5ff20d58d8aec80a07866128cc80538a5248
water-code <168319570@qq.com>
2023-05-20 15:13:53 +0800

    change config

--- xiangchengfront/vite.config.js
+++ xiangchengfront/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteCompression from 'vite-plugin-compression'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  publicPath:'./',
 //在plugins配置数组里添加gzip插件
  plugins: [vue(),viteCompression({
   verbose: true,
