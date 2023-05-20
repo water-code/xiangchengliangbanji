5abcc9b4a2b7a3f90dad872a1c19d0534fb396d1
water-code <168319570@qq.com>
2023-05-20 10:48:01 +0800

    change config

--- xiangchengfront/vite.config.js
+++ xiangchengfront/vite.config.js
@@ -34,6 +34,18 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['/api/captcha?type=math'],
+      
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        //   }
+        // }
+      }
+
     }
   }
 })
