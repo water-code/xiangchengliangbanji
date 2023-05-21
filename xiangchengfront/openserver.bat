8bd78dd18c90b190ec8e9fa5fdcb713a7dcdf958
qkmc <qkmc@outlook.com>
2023-05-22 01:18:31 +0800

    script auto commit

--- xiangchengfront/openserver.bat
+++ xiangchengfront/openserver.bat
@@ -5,6 +5,17 @@ set HOST=xiangcheng.ruankun.xyz
 set USER=root
 set PASSWORD=@Sicau211
 set REMOTE_SCRIPT=/root/xiangchengfront/start.sh
+set COMMIT_MSG="script auto commit"
+set REMOTE_NAME=gitee
+
+echo Adding changes...
+git add .
+
+echo Committing changes with message %COMMIT_MSG%...
+git commit -m %COMMIT_MSG%
+
+echo Pushing changes to remote repository %REMOTE_NAME%...
+git push %REMOTE_NAME%
 
 echo Running script on %HOST%...
 
